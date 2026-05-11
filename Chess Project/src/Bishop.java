c7a5bce1e29364701fb1aa848f0d71390d7d9d2b
nanisettimihir <nanisettimihir307@gmail.com>
2026-05-11 15:33:24 -0700

    dwauibhdiuawhdcacw
    jbiewqd

--- Chess Project/src/Bishop.java
+++ Chess Project/src/Bishop.java
@@ -1,4 +1,4 @@
-/*import java.util.ArrayList;
+import java.util.ArrayList;
 
 
     public class Bishop extends Piece
@@ -26,11 +26,56 @@
             ArrayList<int[]> legalMoves = new ArrayList<>();
 
 
-            int upStart = 0;
+            int downStartRow = row;
+            int downStartCol = col;
+
+            for(int r = row-1; r >= 0; r--) {
+                for (int c = col - 1; c >= 0; c--) {
+
+
+
+                    if (board[r][c] == null) {
+                        downStartRow = r;
+                        downStartCol = c;
+
+                    }
+                    else if (!board[r][c].color.equals(this.color)) {
+                        downStartRow = r;
+                        downStartCol = c;
+                        break;
+                    }
+                    else if (board[r][c].color.equals(this.color)) {
+                        downStartRow = r + 1;
+                        downStartCol = c + 1;
+                    }
+                }
+
+
+            }
+
+            for(int r = downStartRow; r>=0; r--){
+
+                for(int c = downStartCol; c>=0; c--){
+
+                    legalMoves.add(new int[]{r,c});
+
+                }
+
+            }
+
+
+
+            for(int r = row; r<=7; r++){
+
+                for(int c = col; c<=7; c++){
 
 
 
 
+                }
+
+
+            }
 
 
 
